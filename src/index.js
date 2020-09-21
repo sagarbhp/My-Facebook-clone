03991729bd837eaeb009fc9a5840e78a93c48d33
Sagar <sagarbhp@gmail.com>
2020-09-21 00:35:26 -0700

    first commit- added all initial functionality

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { StateProvider } from './StateProvider/StateProvider';
+import reducer, { initialState } from './StateProvider/reducer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState}
+      reducer={reducer}
+    >
+          <App />
+    </StateProvider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
